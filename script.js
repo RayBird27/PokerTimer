ba223289c308b94e0e8402fb0ba9b0c65083427f
RayBird27 <meza.ramon27@gmail.com>
2023-08-04 15:30:19 -0700

    Subject: Adding Logic Description: Adding mp3 sound

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 let timerInterval;
 let seconds = 0;
+let initialTime = 0;
+let isFlashing = false;
 
 const timerElement = document.getElementById('timer');
 const startButton = document.getElementById('start');
 const resetButton = document.getElementById('reset');
+const inputTime = document.getElementById('inputTime');
 
 function startTimer() {
     if (!timerInterval) {
+        initialTime = parseInt(inputTime.value) * 60;
+        seconds = initialTime;
         timerInterval = setInterval(updateTimer, 1000);
         startButton.disabled = true;
+        inputTime.disabled = true;
     }
 }
 
@@ -18,11 +24,24 @@ function resetTimer() {
     seconds = 0;
     timerElement.textContent = formatTime(seconds);
     startButton.disabled = false;
+    inputTime.disabled = false;
+    isFlashing = false;
+    document.body.style.backgroundColor = '#f0f0f0';
 }
 
 function updateTimer() {
-    seconds++;
-    timerElement.textContent = formatTime(seconds);
+    if (seconds > 0) {
+        seconds--;
+        timerElement.textContent = formatTime(seconds);
+    } else {
+        clearInterval(timerInterval);
+        timerInterval = null;
+        timerElement.textContent = 'Time\'s Up!';
+        flashBackground();
+        playAlarm();
+        startButton.disabled = false;
+        isFlashing = true;
+    }
 }
 
 function formatTime(timeInSeconds) {
@@ -33,6 +52,18 @@ function formatTime(timeInSeconds) {
 
 function padZero(number) {
     return number < 10 ? `0${number}` : number;
+}
+
+function flashBackground() {
+    if (isFlashing) {
+        document.body.style.backgroundColor = document.body.style.backgroundColor === '#f0f0f0' ? 'red' : '#f0f0f0';
+        setTimeout(flashBackground, 500);
+    }
+}
+
+function playAlarm() {
+    const audio = new Audio('alarm.mp3');
+    audio.play();
 }
 
 startButton.addEventListener('click', startTimer);
